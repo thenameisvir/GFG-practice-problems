21a4f5aad1b2070566f53da2439b209a44d9f74f
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2024-12-05 20:51:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Diagonal Tree Traversal/diagonal-tree-traversal.cpp
+++ Difficulty: Medium/Diagonal Tree Traversal/diagonal-tree-traversal.cpp
@@ -126,30 +126,25 @@ struct Node
 
 vector<int> diagonal(Node *root)
 {
-    vector<int>v;
-    if(!root) return v;
-    queue<Node*>q;
-    
-    q.push(root);
-    while(!q.empty()){
-        
-        Node* temp = q.front();
-        q.pop();
-        
-        while(temp){
-            v.push_back(temp->data);
-            if(temp->left){
-                // baad me dekhenge 
-                q.push(temp->left);
-            }
-            
-            temp = temp->right;
-        }
-        
-        
-    }
-    
-    
-    return v;
-    
+   vector<int>v;
+   if(!root) return v;
+   
+   queue<Node*>q;
+   
+   q.push(root);
+   
+   while(!q.empty()){
+       Node* temp = q.front();q.pop();
+       
+       while(temp){
+           v.push_back(temp->data);
+           if(temp->left){
+               q.push(temp->left);
+           }
+           
+           temp = temp->right;
+       }
+   }
+   
+   return v;
 }
