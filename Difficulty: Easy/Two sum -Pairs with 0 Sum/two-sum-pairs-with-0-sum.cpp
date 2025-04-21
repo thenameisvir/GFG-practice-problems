a1461b04a094fbe87d4043575681099dec85e7ce
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-04-21 22:08:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two sum -Pairs with 0 Sum/two-sum-pairs-with-0-sum.cpp
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+class Matrix {
+  public:
+    template <class T>
+    static void input(vector<vector<T>> &A, int n, int m) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                scanf("%d ", &A[i][j]);
+            }
+        }
+    }
+
+    template <class T>
+    static void print(vector<vector<T>> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            for (int j = 0; j < A[i].size(); j++) {
+                cout << A[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+
+// } Driver Code Ends
+
+// User function template for C++
+
+class Solution {
+  public:
+    vector<vector<int>> getPairs(vector<int>& arr) {
+        unordered_set<int> s;
+        set<vector<int>> res;
+
+        for (int num : arr) {
+            if (s.count(-num)) {
+                int a = num, b = -num;
+                if (a > b) swap(a, b);
+                res.insert({a, b});
+            }
+            s.insert(num);
+        }
+
+        return vector<vector<int>>(res.begin(), res.end());
+    }
+};
+
+
+
+//{ Driver Code Starts.
+//Back-end complete function Template for C++
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n = arr.size();
+        // char c;cin>>c;
+        Solution obj;
+        vector<vector<int>> res = obj.getPairs(arr);
+        if (res.size() == 0) {
+            cout << endl;
+        } else
+            Matrix::print(res);
+        cout << "~" << endl;
+    }
+}
+
+// } Driver Code Ends
