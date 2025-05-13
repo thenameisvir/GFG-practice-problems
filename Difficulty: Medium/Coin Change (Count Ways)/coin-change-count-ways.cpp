e7509fa0fba432bf5797de1bcadc42ef7b639418
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-05-13 11:35:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int solve(vector<int>& coins, int sum, int i, vector<vector<int>>& dp){
+        if(sum == 0) return 1;
+        if(i >= coins.size() || sum < 0) return 0;
+        
+        if(dp[sum][i] != -1) return dp[sum][i];
+        
+        int inc = solve(coins, sum - coins[i], i, dp);    // include current coin
+        int exc = solve(coins, sum, i + 1, dp);           // exclude current coin
+        
+        dp[sum][i] = inc + exc;
+        return dp[sum][i];
+    }
+    
+    int count(vector<int>& coins, int sum) {
+        vector<vector<int>> dp(sum + 1, vector<int>(coins.size(), -1));
+        return solve(coins, sum, 0, dp);
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int sum;
+        cin >> sum;
+        cin.ignore();
+        Solution ob;
+        cout << ob.count(arr, sum) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
