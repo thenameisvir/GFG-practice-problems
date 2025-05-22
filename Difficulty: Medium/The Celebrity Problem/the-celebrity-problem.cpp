b0988ad0fadfe13890602c78b2517d3b57757cc6
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-05-22 08:52:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -1,75 +1,32 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
   public:
-    // Function to find if there is a celebrity in the party or not.
-    int celebrity(vector<vector<int> >& M) {
-        int n = M.size();
-        
-        stack<int>st;
-        
-        for(int i=0;i<n;i++){
-            st.push(i);
-        }
-        
-        while(st.size()!=1){
-            int a = st.top();st.pop();
-            int b = st.top();st.pop();
+    int celebrity(vector<vector<int> >& arr) {
+        // code here
+        int cel = -1;
+        for(int i=0;i<arr.size();i++){
+            bool check = true;
+            for(int j=0;j<arr.size();j++){
+                if(arr[i][j]==1 && i!=j){
+                    check = false;
+                    break;
+                }
+            }
             
-            if(M[a][b]){
-                st.push(b);
+            if(check){
+                for(int k = 0;k<arr.size();k++){
+                if(arr[k][i]==0){
+                    check = false;
+                    break;
+                }
             }
-            else{
-                st.push(a);
             }
+            
+            if(check){
+                cel = i;
+            }
+            
         }
         
-        int mightBeCel = st.top();
-        
-        for(int i=0;i<n;i++){
-            if(M[mightBeCel][i]!=0){
-                return -1;
-            }
-        }
-        
-        for(int i=0;i<n;i++){
-            if(M[i][mightBeCel]!=1 && i!=mightBeCel){
-                return -1;
-            }
-        }
-        
-        
-        
-        return mightBeCel;
+        return cel;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<vector<int> > M(n, vector<int>(n, 0));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                cin >> M[i][j];
-            }
-        }
-        Solution ob;
-        cout << ob.celebrity(M) << endl;
-        cout << "~" << endl;
-    }
-}
-
-// } Driver Code Ends
