2794c3cf94f19c66e860a09df66127d2d94464e8
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-05-28 16:31:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Interleave the First Half of the Queue with Second Half/interleave-the-first-half-of-the-queue-with-second-half.cpp
+++ Difficulty: Medium/Interleave the First Half of the Queue with Second Half/interleave-the-first-half-of-the-queue-with-second-half.cpp
@@ -1,73 +1,37 @@
-//{ Driver Code Starts
-//Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-//User function Template for C++
-  
-class Solution{
-public:
-    
-    vector<int> rearrangeQueue(queue<int> &q){
-        queue<int>second;
+class Solution {
+  public:
+    queue<int> rearrangeQueue(queue<int> q) {
+        int n = q.size() / 2;
+        queue<int> firstHalf;
         
-        int s = q.size();
-        
-        int k = s/2;
-        
-        while(k){
-            int f = q.front();
+        // Step 1: Store first half
+        for (int i = 0; i < n; i++) {
+            firstHalf.push(q.front());
             q.pop();
-            second.push(f);
-            k--;
         }
-        
-        vector<int>v;
-        
-        while(!q.empty() && !second.empty()){
-            int two = second.front();
-            second.pop();
-            int one = q.front();
+
+        queue<int> result;
+
+        // Step 2: Alternate from firstHalf and remaining q
+        while (!firstHalf.empty() && !q.empty()) {
+            result.push(firstHalf.front());
+            firstHalf.pop();
+
+            result.push(q.front());
             q.pop();
-            v.push_back(two);
-            v.push_back(one);
         }
-        
-        
-        return v;
-        
+
+        // Step 3: If any leftovers (odd size case)
+        while (!firstHalf.empty()) {
+            result.push(firstHalf.front());
+            firstHalf.pop();
+        }
+
+        while (!q.empty()) {
+            result.push(q.front());
+            q.pop();
+        }
+
+        return result;
     }
 };
-
-//{ Driver Code Starts.
-
-int main(){
-    
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        
-        queue<int> q;
-        for(int i=0;i<n;i++){
-            int a;
-            cin>>a;
-            q.push(a);
-        }
-        Solution ob;
-        vector<int> ans=ob.rearrangeQueue(q);
-        for(int i=0;i<ans.size();i++){
-            cout<<ans[i]<<" ";
-        }
-        cout<<endl;
-    
-cout << "~" << "\n";
-}
-    
-    return 0;
-}
-// } Driver Code Ends
