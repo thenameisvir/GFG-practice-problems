a99acdf1f36cf7fb140e37285d01648393b97cd4
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-05-28 17:59:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.cpp
+++ Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.cpp
@@ -1,95 +1,35 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-
 class Solution {
   public:
-    vector<int> FirstNegativeInteger(vector<int>& arr, int k) {
-        queue<int>q;
-        
-        vector<int>v;
-        
-        // pehla kaam hai k pehle window ko process krdo
-        
-        for(int i=0;i<k;i++){
-            int num = arr[i];
-            
-            if(num<0){
+    vector<int> firstNegInt(vector<int>& arr, int k) {
+        queue<int> q;
+        vector<int> v;
+
+        // Process first window
+        for (int i = 0; i < k; i++) {
+            if (arr[i] < 0)
                 q.push(i);
-            }
         }
-        
-        // now process the answer of first window into the vector
-        
-        if(!q.empty()){
+
+        if (!q.empty())
             v.push_back(arr[q.front()]);
-        }
-        else{
+        else
             v.push_back(0);
-        }
-        
-        
-        // now process the other windows 
-        
-        for(int i=k;i<arr.size();i++){
-            // check karo kahi out of bound to nahi hai
-            if(!q.empty() && q.front()<=i-k){
+
+        // Process rest of the windows
+        for (int i = k; i < arr.size(); i++) {
+            // Remove out-of-window indices
+            while (!q.empty() && q.front() <= i - k)
                 q.pop();
-            }
-            
-            // now process the current element 
-            // bhai iski bhi to izzat krle kya pta negative ho 
-            
-            if(arr[i]<0){
+
+            if (arr[i] < 0)
                 q.push(i);
-            }
-            
-            
-            if(!q.empty()){
-            v.push_back(arr[q.front()]);
+
+            if (!q.empty())
+                v.push_back(arr[q.front()]);
+            else
+                v.push_back(0);
         }
-        else{
-            v.push_back(0);
-        }
-        }
-        
-        
+
         return v;
-        
-        
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        Solution ob;
-        vector<int> ans = ob.FirstNegativeInteger(arr, k);
-        for (auto it : ans) {
-            cout << it << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
