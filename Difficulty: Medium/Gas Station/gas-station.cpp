fb9ba08a00a6a0b28e07622df602ceb183851371
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-05-30 14:33:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Gas Station/gas-station.cpp
+++ Difficulty: Medium/Gas Station/gas-station.cpp
@@ -1,74 +1,44 @@
-//{ Driver Code Starts
-// C++ program to find union of
-// two sorted arrays
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 /*You are required to complete this method*/
 class Solution {
   public:
     int startStation(vector<int> &gas, vector<int> &cost) {
-       int deficit = 0;
+        int n = gas.size();
+        int front = 0;
+        int rear = 0;
         int balance = 0;
-        int start = 0;
-
-        for(int i=0;i<gas.size();i++){
-            balance = balance + gas[i] - cost[i];
-
-            if(balance<0){
-                deficit+=abs(balance);
-                start=i+1;
+        
+        while(true){
+            // petrol check kar gadi me kitna hai 
+            balance+=gas[rear]-cost[rear];
+            // ab jitna petrol hai uske hisab se check karenge kaha jana h
+            if(balance>=0){
+                // isme bar rear aage badhana hota hai 
+                rear = (rear+1)%n;
+                
+                // check karle kahi answer tak pohoch toh nahi gaye 
+                if(rear==front){
+                    return front;
+                }
+            }
+            else{
+                // yaha apna jo fuel hai vo negative aya hai 
+                // toh check kar kahi mai negative me hokr chakkar toh nhi laga aya hu
+                if(front>rear){
+                    return -1;
+                }
+                
+                // isme front update hoga
+                front = (rear+1)%n;
+                rear = front;
                 balance = 0;
+                
+                if(front==0){
+                    return -1;
+                }
             }
         }
-
-        if(balance-deficit>=0){
-            return start;
-        }
-        else{
-            return -1;
-        }
+        
+        return -1;
+        
     }
 };
-
-
-//{ Driver Code Starts.
-/* Driver program to test above function */
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-
-    while (t--) {
-
-        vector<int> gas, cost;
-        string input1;
-        getline(cin, input1);
-        stringstream ss(input1);
-        int number1;
-        while (ss >> number1) {
-            gas.push_back(number1);
-        }
-        string input2;
-        getline(cin, input2);
-        stringstream sss(input2);
-        int number2;
-        while (sss >> number2) {
-            cost.push_back(number2);
-        }
-        Solution ob;
-        int ans = ob.startStation(gas, cost);
-
-        cout << ans;
-
-        cout << endl;
-        cout << "~\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
