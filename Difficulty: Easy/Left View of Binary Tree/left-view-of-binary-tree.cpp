78606fc7017c265ec0ee0ed6dcc32640c4918707
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-06-18 16:09:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.cpp
+++ Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.cpp
@@ -1,16 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-// Tree Node
-struct Node {
-    int data;
-    Node* left;
-    Node* right;
-};
-
-
-// } Driver Code Ends
 /* A binary tree node
 
 struct Node
@@ -28,129 +15,32 @@ struct Node
 
 class Solution {
   public:
-    void ans(Node *root,vector<int>&v,int level){
-        // if null hai to vapas chale jao
-        if(root==NULL) return;
-        
-        if(v.size()==level){
-            v.push_back(root->data);
+    vector<int> leftView(Node *root) {
+        queue<Node*>q;
+        q.push(root);
+        vector<int>vir;
+        if(!root) return vir;
+        while(!q.empty()){
+            int size = q.size();
+            bool flag = false;
+            for(int i=0;i<size;i++){
+                if(!flag){
+                    int element = q.front()->data;
+                    vir.push_back(element);
+                    flag = true;
+                }
+                
+                if(q.front()->left){
+                    q.push(q.front()->left);
+                }
+                if(q.front()->right){
+                    q.push(q.front()->right);
+                }
+                
+                q.pop();
+            }
         }
         
-        
-        ans(root->left,v,level+1);
-        ans(root->right,v,level+1);
-        
-        
-    }
-    vector<int> leftView(Node *root) {
-     
-     vector<int>v;
-     ans(root,v,0);
-     
-     
-     return v;
-        
+        return vir;
     }
 };
-
-//{ Driver Code Starts.
-
-// Utility function to create a new Tree Node
-Node* newNode(int val) {
-    Node* temp = new Node;
-    temp->data = val;
-    temp->left = NULL;
-    temp->right = NULL;
-
-    return temp;
-}
-
-// Function to Build Tree
-Node* buildTree(string str) {
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N')
-        return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for (string str; iss >> str;)
-        ip.push_back(str);
-
-    // for(string i:ip)
-    //     cout<<i<<" ";
-    // cout<<endl;
-    // Create the root of the tree
-    Node* root = newNode(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size()) {
-
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-
-        // Get the current node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if (currVal != "N") {
-
-            // Create the left child for the current node
-            currNode->left = newNode(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size())
-            break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N") {
-
-            // Create the right child for the current node
-            currNode->right = newNode(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
-}
-
-int main() {
-    int t;
-    scanf("%d ", &t);
-    while (t--) {
-        string s;
-        getline(cin, s);
-        Node* root = buildTree(s);
-        Solution ob;
-        vector<int> vec = ob.leftView(root);
-        if (vec.size() == 0) {
-            cout << "[]";
-        }
-        for (int x : vec)
-            cout << x << " ";
-        cout << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends
