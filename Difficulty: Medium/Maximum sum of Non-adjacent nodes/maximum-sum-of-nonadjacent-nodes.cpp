aabd70233e5c2ec078df51b169f0ed386cdad188
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-06-22 13:03:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum sum of Non-adjacent nodes/maximum-sum-of-nonadjacent-nodes.cpp
+++ Difficulty: Medium/Maximum sum of Non-adjacent nodes/maximum-sum-of-nonadjacent-nodes.cpp
@@ -1,97 +1,4 @@
-//{ Driver Code Starts
-//Initial Template for C++
-
-#include<bits/stdc++.h> 
-using namespace std; 
-
-// Tree Node
-struct Node
-{
-    int data;
-    Node* left;
-    Node* right;
-};
-
-// Utility function to create a new Tree Node
-Node* newNode(int val)
-{
-    Node* temp = new Node;
-    temp->data = val;
-    temp->left = NULL;
-    temp->right = NULL;
-    
-    return temp;
-}
-
-// Function to Build Tree
-Node* buildTree(string str)
-{   
-    // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
-            return NULL;
-    
-    // Creating vector of strings from input 
-    // string after spliting by space
-    vector<string> ip;
-    
-    istringstream iss(str);
-    for(string str; iss >> str; )
-        ip.push_back(str);
-        
-    // Create the root of the tree
-    Node* root = newNode(stoi(ip[0]));
-        
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-        
-    // Starting from the second element
-    int i = 1;
-    while(!queue.empty() && i < ip.size()) {
-            
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-            
-        // Get the current node's value from the string
-        string currVal = ip[i];
-            
-        // If the left child is not null
-        if(currVal != "N") {
-                
-            // Create the left child for the current node
-            currNode->left = newNode(stoi(currVal));
-                
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-            
-        // For the right child
-        i++;
-        if(i >= ip.size())
-            break;
-        currVal = ip[i];
-            
-        // If the right child is not null
-        if(currVal != "N") {
-                
-            // Create the right child for the current node
-            currNode->right = newNode(stoi(currVal));
-                
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-    
-    return root;
-}
-
-
-// } Driver Code Ends
-//User function Template for C++
-
-//Node Structure
+// Node Structure
 /*
 struct Node
 {
@@ -101,47 +8,29 @@ struct Node
 };
 */
 
-class Solution{
+class Solution {
   public:
-    //Function to return the maximum sum of non-adjacent nodes.
-    pair<int,int> maxi(Node* root){
-        if(!root) return {0,0};
+    
+    pair<int,int>IncludeExclude(Node* root){
+        if(!root){
+            return {0,0};
+        }
         
-        auto lsum = maxi(root->left);
-        auto rsum = maxi(root->right);
+        auto lsum = IncludeExclude(root->left);
+        auto rsum = IncludeExclude(root->right);
         
-        // sum including the node okayy
-        int included = root->data + lsum.second + rsum.second;
+        int inc = max(lsum.first,lsum.second) + max(rsum.first,rsum.second);
+        int exc = root->data + lsum.first + rsum.first;
         
-        // sum excluding the node
-        int excluded = max(lsum.first,lsum.second) + max(rsum.first,rsum.second);
-        
-        return {included,excluded};
+        return {inc,exc};
     }
-    int getMaxSum(Node *root) 
-    {
-        auto ans = maxi(root);
+    
+    int getMaxSum(Node *root) {
+        
+        auto ans = IncludeExclude(root);
         return max(ans.first,ans.second);
-    }
+            
+        }
 };
 
-//{ Driver Code Starts.
 
-// Driver code 
-int main()
-{
-  int t;
-  scanf("%d ",&t);
-  while (t--)
-  {
-        string s;
-		getline(cin,s);
-		Node* root = buildTree(s);
-		Solution ob;
-        cout<<ob.getMaxSum(root)<<endl;
-  
-cout << "~" << "\n";
-}
-  return 0;
-}
-// } Driver Code Ends
