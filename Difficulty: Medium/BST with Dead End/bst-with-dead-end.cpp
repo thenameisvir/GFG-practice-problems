a5846937b4c4329ee583ca3795b3c271c6891360
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-07-02 18:07:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/BST with Dead End/bst-with-dead-end.cpp
+++ Difficulty: Medium/BST with Dead End/bst-with-dead-end.cpp
@@ -1,128 +1,53 @@
-//{ Driver Code Starts
-//Initial template for C++
-
-#include<bits/stdc++.h>
-
-using namespace std;
-
-struct Node
-{
-	int data;
-	struct Node *left;
-	struct Node *right;
-	
-	Node(int x){
-	    data = x;
-	    left = NULL;
-	    right = NULL;
-	}
-};
-
-
-void insert(Node ** tree, int val)
-{
-    Node *temp = NULL;
-    if(!(*tree))
-    {
-        temp = new Node(val);
-        *tree = temp;
-        return;
-    }
-
-    if(val < (*tree)->data)
-    {
-        insert(&(*tree)->left, val);
-    }
-    else if(val > (*tree)->data)
-    {
-        insert(&(*tree)->right, val);
-    }
-
-}
-
-
-
-
-int getCountOfNode(Node *root, int l, int h)
-{
-
-    if (!root) return 0;
-
-
-    if (root->data == h && root->data == l)
-        return 1;
-
-    if (root->data <= h && root->data >= l)
-         return 1 + getCountOfNode(root->left, l, h) +
-                    getCountOfNode(root->right, l, h);
-
-    else if (root->data < l)
-         return getCountOfNode(root->right, l, h);
-
-
-    else return getCountOfNode(root->left, l, h);
-}
-
-
-// } Driver Code Ends
 /*The Node structure is
-struct Node {
-int data;
-Node * right, * left;
+class Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x){
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
 };*/
 
-/*You are required to complete below method */
-class Solution{
+class Solution {
   public:
-    bool checkDeadEnd(Node* root, int min, int max) {
-        if (!root) return false;
-
-        // Check if this is a dead end
-        if (!root->left && !root->right) {
-            if (min == max) return true;
+    void solve(Node* root, vector<int>& leaves){
+        if(!root) return;
+        if(!root->left && !root->right){
+            leaves.push_back(root->data);
+            return;
         }
-
-        // Recur for left and right subtrees
-        return checkDeadEnd(root->left, min, root->data - 1) ||
-               checkDeadEnd(root->right, root->data + 1, max);
+        solve(root->left, leaves);
+        solve(root->right, leaves);
     }
 
-    bool isDeadEnd(Node* root) {
-        return checkDeadEnd(root, 1, INT_MAX);
+    bool search(Node* root, int val){
+        if(!root) return false;
+        if(root->data == val) return true;
+        if(val < root->data) return search(root->left, val);
+        else return search(root->right, val);
+    }
+
+    bool isDeadEnd(Node *root) {
+        vector<int> leaves;
+        solve(root, leaves);
+        
+        for(int val : leaves){
+            if(val == 1){
+                if(search(root, 2)) return true;
+                continue; // to avoid checking val-1 = 0
+            }
+            if(search(root, val - 1) && search(root, val + 1)){
+                return true;
+            }
+        }
+        
+        return false;
     }
 };
 
-//{ Driver Code Starts.
-// bool isDeadEnd(Node *root);
-int main()
-{
-    int T;
-    cin>>T;
-    while(T--)
-    {
-        Node *root;
-        Node *tmp;
-    //int i;
-
-        root = NULL;
-
-        int N;
-        cin>>N;
-        for(int i=0;i<N;i++)
-        {
-            int k;
-            cin>>k;
-            insert(&root, k);
-
-        }
-
-     Solution ob;
-     cout<<ob.isDeadEnd(root);
-     cout<<endl;
-    
-cout << "~" << "\n";
-}
-}
 
 
-// } Driver Code Ends
