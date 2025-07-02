c15e0fb8ac30c306ad55b3a4995869a6c57b9150
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-07-02 19:37:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Brothers From Different Roots/brothers-from-different-roots.cpp
+++ Difficulty: Easy/Brothers From Different Roots/brothers-from-different-roots.cpp
@@ -1,89 +1,3 @@
-//{ Driver Code Starts
-//Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node
-{
-    int data;
-    struct Node *left;
-    struct Node *right;
-
-    Node(int val) {
-        data = val;
-        left = right = NULL;
-    }
-};
-
-// Function to Build Tree
-Node* buildTree(string str)
-{
-    // Corner Case
-    if (str.length() == 0 || str[0] == 'N')
-        return NULL;
-
-    // Creating vector of strings from input
-    // string after spliting by space
-    vector<string> ip;
-
-    istringstream iss(str);
-    for (string str; iss >> str; )
-        ip.push_back(str);
-
-    // Create the root of the tree
-    Node *root = new Node(stoi(ip[0]));
-
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-
-    // Starting from the second element
-    int i = 1;
-    while (!queue.empty() && i < ip.size()) {
-
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-
-        // Get the current node's value from the string
-        string currVal = ip[i];
-
-        // If the left child is not null
-        if (currVal != "N") {
-
-            // Create the left child for the current Node
-            currNode->left = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-
-        // For the right child
-        i++;
-        if (i >= ip.size())
-            break;
-        currVal = ip[i];
-
-        // If the right child is not null
-        if (currVal != "N") {
-
-            // Create the right child for the current node
-            currNode->right = new Node(stoi(currVal));
-
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-
-    return root;
-}
-
-
-// } Driver Code Ends
-//User function Template for C++
-
 /*Structure of the Node of the BST is as
 struct Node
 {
@@ -98,76 +12,33 @@ struct Node
 };
 */
 
-class Solution
-{
-public:
-    void BuildIt(Node* root1,vector<int>&v){
-        if(!root1) return;
-        
-        BuildIt(root1->left,v);
-        v.push_back(root1->data);
-        BuildIt(root1->right,v);
+class Solution {
+  public:
+    void solve(Node* root, vector<int>& v){
+        if(!root) return;
+        solve(root->left, v);
+        v.push_back(root->data);
+        solve(root->right, v);
     }
-    void BuildIt2(Node* root2, vector<int>& v2) {
-    if (!root2) return;
-    
-    BuildIt2(root2->right, v2); // Corrected from BuildIt to BuildIt2
-    v2.push_back(root2->data);
-    BuildIt2(root2->left, v2); // Corrected from BuildIt to BuildIt2
-}
-    int countPairs(Node* root1, Node* root2, int x)
-    {
-        vector<int>v;
-        vector<int>v2;
-        BuildIt(root1,v);
-        BuildIt2(root2,v2);
-        
+
+    int countPairs(Node* root1, Node* root2, int x) {
+        vector<int> temp1, temp2;
+        solve(root1, temp1);  // ✅ fix: use root1
+        solve(root2, temp2);  // ✅ fix: use root2
+
+        unordered_map<int, bool> mp;
+        for(int val : temp1){
+            mp[val] = true;
+        }
+
         int count = 0;
-        
-        int p1 = 0;
-        int p2 = 0;
-        while(p1<v.size()&&p2<v2.size()){
-            if(v[p1]+v2[p2] == x){
+        for(int val : temp2){
+            int complement = x - val;
+            if(mp.find(complement) != mp.end()){
                 count++;
-                p1++;
-                p2++;
-            }
-            else if(v[p1]+v2[p2] > x){
-                p2++;
-            }
-            else if(v[p1]+v2[p2] < x){
-                p1++;
             }
         }
-        
+
         return count;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main()
-{
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--)
-    {
-        string tree1, tree2;
-        getline(cin, tree1);
-        getline(cin, tree2);
-        Node* root1 = buildTree(tree1);
-        Node* root2 = buildTree(tree2);
-        int x;
-        cin >> x;
-        cin.ignore();
-        Solution ob;
-        cout << ob.countPairs(root1, root2, x) << "\n";
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-
-// } Driver Code Ends
