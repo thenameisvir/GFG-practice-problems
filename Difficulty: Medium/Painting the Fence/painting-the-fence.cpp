69503f7ab71ff0cf2b157dd2f9a5737fb1216c16
Madhur Solanki <117061327+thenameisvir@users.noreply.github.com>
2025-07-16 09:28:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Painting the Fence/painting-the-fence.cpp
+++ Difficulty: Medium/Painting the Fence/painting-the-fence.cpp
@@ -1,101 +1,45 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-#include <math.h>
-#include <stdio.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    int usingRec(int n,int k){
-        
-        // base case ek toh smjh aa rha hai ki
-        if(n==1) return k; // kyuki aur koi choice hi nahi hai apne pas
-        
-        if(n==2) return k+k*(k-1);
-        
-        // dono base case smjh aagye hain ab ans build karte hai chalo
-        
-        int ans = (usingRec(n-1,k) + usingRec(n-2,k))*(k-1);
-        
-        return ans;
-        
-        
+    int solve(int n,int k,vector<int>&dp){
+        if(n==1) return k;
+        if(n==2) return k+(k*(k-1)); // very important observation
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n] =  (k-1)*(solve(n-1,k,dp) + solve(n-2,k,dp));
     }
     
-    int usingMemo(int n,int k,vector<int>&dp){
-        if(n==1) return k;
+    int tab(int n,int k){
+     
+     vector<int>dp(n+1,-1);
+     dp[1] = k;
+     dp[2] = k+(k*(k-1));
+     
+     for(int i=3;i<=n;i++){
+         dp[i] = (k-1)*(dp[i-1] + dp[i-2]);
+     }
+     
+     return dp[n];
+     
         
-        if(n==2) return k+k*(k-1);
-        // copy paste base case as it is
-        
-        if(dp[n]!=-1) return dp[n];
-        
-        int ans = (usingMemo(n-1,k,dp) + usingMemo(n-2,k,dp))*(k-1);
-        
-        dp[n] = ans;
-        return dp[n];
     }
-    int usingTab(int n, int k){
-        vector<int>dp(n+1,-1);
-        dp[1] = k;
-        dp[2] = k+k*(k-1);
+    int space(int n, int k){
+        if(n==1) return k;
+        if(n==2) return k+(k*(k-1));
+        int prev = k;
+        int curr = k+(k*(k-1));
         
         for(int i=3;i<=n;i++){
-        dp[i] = (dp[i-1] + dp[i-2])*(k-1);  
-        
+            int temp = (k-1)*(prev + curr);
+            prev = curr;
+            curr = temp;
         }
         
-        return dp[n];
-    }
-    int spaceOptimised(int n, int k){
+        return curr;
         
-        
-         int prev1 = k;
-         int prev2 = k+k*(k-1);
-         
-         if(n==1) return k;
-         if(n==2) return prev2;
-         int curr;
-         for(int i=3;i<=n;i++){
-             curr = (prev1+prev2)*(k-1);
-             prev1 = prev2;
-             prev2 = curr;
-         }
-         
-         return curr;
-        
-        
-        
-    }
+    } 
     int countWays(int n, int k) {
-        
-        // int ansOfRec = usingRec(n,k); return ansOfRec;
-        // vector<int>dp(n+1,-1);
-        // int ans = usingMemo(n,k,dp); return ans;
-        // int ans = usingTab(n,k); return ans;
-        int ans = spaceOptimised(n,k); return ans;
-        
-        
+        vector<int>dp(n+1,-1);
+        return space(n,k);
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, k;
-        cin >> n >> k;
-        Solution ob;
-        cout << ob.countWays(n, k) << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
